ed9b37214db8638cce13022008dffd3d16db5b3f
BenDaBanana <133304730+BenDaBanana@users.noreply.github.com>
2023-06-09 15:14:14 +1200

    Add files via upload

--- Project/js/border.js
+++ Project/js/border.js
@@ -1,22 +1,40 @@
-var obstacleMover = 0
-function Border (x, y, width, height, type){
-	this.x = x
-	this.y = y
-	this.width = width
-	this.height = height
-	this.type = type
-	
-	this.draw = function() {
-		if(this.type === 1){
-			ctx.fillStyle = "blue"
-		} else if (this.type === 2) {
-			ctx.fillStyle = "red"
-		} else if (this.type === 3){
-			ctx.fillStyle = "orange"
-		}
-		this.x -= obstacleMover
-		ctx.fillRect(this.x, this.y, this.width, this.height)
-	}
-}
+var obstacleMover = 0;
 
-    
+function Border(x, y, width, height, typee) {
+	
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.typee = typee;
+
+
+
+
+	this.draw = function() {
+		if (this.typee === 1) {
+			ctx.fillStyle = "blue";
+		} else if (this.typee === 2) {
+			ctx.fillStyle = "red";
+		} else if (this.typee === 3) {
+			ctx.fillStyle = "orange";
+			
+		}
+		this.x -= obstacleMover;
+		if (hasDoubleJump == true){
+				if (this.typee != 3){
+					if (active == true){
+					ctx.fillRect(this.x, this.y, this.width, this.height);
+					}
+				
+				}
+			
+		}else{
+			if (active == true){
+			ctx.fillRect(this.x, this.y, this.width, this.height)
+			}
+			
+		}
+		
+	};
+}
--- Project/js/player.js
+++ Project/js/player.js
@@ -1,139 +1,129 @@
-function Player(x, y) {
-    // player variables
-	var jumpHeight = 45
-	var doubleJumpHeight = 40
-	var isGrounded = false
-	var canDoubleJump = true
-	var hasDoubleJump = true
-	
-    this.x = x;
-    this.y = y;
-    this.xspeed = 0;
-    this.yspeed = 0;
-    this.friction = 0.4;
-    this.maxSpeed = 15;
-	this.yMaxspeed = 20
-    this.width = 50;
-    this.height = 100;
-    this.active = true; // Add the 'active' property and set it to true
+var jumpHeight = 45;
+var doubleJumpHeight = 40;
+var isGrounded = false;
+var canDoubleJump = true;
+var hasDoubleJump = false;
+class Player {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.xspeed = 0;
+		this.yspeed = 0;
+		this.friction = 0.4;
+		this.maxSpeed = 15;
+		this.yMaxspeed = 40;
+		this.width = 50;
+		this.height = 100;
+		this.active = true;
+	}
 
-    this.step = function() {
-        // movement
-        if (this.active) {
-            // horizontal movement
-            if (!leftKey && !rightKey) {
-                // slow down
-                this.xspeed *= this.friction;
-            } else if (rightKey) {
-                this.xspeed = this.xspeed + 3
-				
-            } else if (leftKey) {
-                this.xspeed = this.xspeed - 3
-            }
-            // vertical movement
+	step() {
+		if (this.y > 720){
+			this.active = false
+			this.maxspeed = 0
+			obstacleMover = 0
+			this.gameOver()
+			active = false
+			
+		}
+			
+		if (this.active) {
+			if (!leftKey && !rightKey) {
+				this.xspeed *= this.friction;
+			} else if (rightKey) {
+				this.xspeed = this.xspeed + 3;
+			} else if (leftKey) {
+				this.xspeed = this.xspeed - 3;
+			}
+
 			if (upKey) {
-				console.log(upKey)
-				//check player grounded
-				
-				if(isGrounded == true){
-				    this.yspeed = -jumpHeight
-				    isGrounded = false
-					upKey = false
-				}else if(hasDoubleJump == true){
-					if(canDoubleJump == true){
-					this.yspeed = -doubleJumpHeight
-				    canDoubleJump = false
+				if (isGrounded === true) {
+					this.yspeed = -jumpHeight;
+					isGrounded = false;
+					upKey = false;
+				} else if (hasDoubleJump === true) {
+					if (canDoubleJump === true) {
+						this.yspeed = -doubleJumpHeight;
+						canDoubleJump = false;
 					}
 				}
-			
-				
-				
 			}
-			//gravity
-			this.yspeed += 5
-			
-            // correct speed
-			if (this.xspeed >this.maxSpeed) {
-				this.xspeed = this.maxSpeed
-			}else if (this.xspeed < -this.maxSpeed) {
-				this.xspeed = -this.maxSpeed
+
+			this.yspeed += 5;
+            isGrounded = false
+			if (this.xspeed > this.maxSpeed) {
+				this.xspeed = this.maxSpeed;
+			} else if (this.xspeed < -this.maxSpeed) {
+				this.xspeed = -this.maxSpeed;
 			}
-				if (this.yspeed >this.yMaxSpeed) {
-				this.yspeed = this.yMaxSpeed
-			}else if (this.yspeed < -this.yMaxSpeed) {
-				this.yspeed = -this.yMaxSpeed
+			if (this.yspeed > this.yMaxspeed) {
+				this.yspeed = this.yMaxspeed;
 			}
-			
-			
-			if(this.xspeed > 0) {
-				this.xspeed = Math.floor(this.xspeed)
-			}else {
-				this.xspeed = Math.ceil(this.xspeed)
+
+			if (this.xspeed > 0) {
+				this.xspeed = Math.floor(this.xspeed);
+			} else {
+				this.xspeed = Math.ceil(this.xspeed);
 			}
-			if(this.yspeed > 0) {
-				this.yspeed = Math.floor(this.yspeed)
-			}else {
-				this.yspeed = Math.ceil(this.yspeed)
+			if (this.yspeed > 0) {
+				this.yspeed = Math.floor(this.yspeed);
+			} else {
+				this.yspeed = Math.ceil(this.yspeed);
 			}
-			//horizontal collision
+
 			let horizontalRect = {
-				
 				x: this.x + this.xspeed,
 				y: this.y,
 				width: this.width,
-				height: this.height
-			}
-			
-			//vertiacal collision
-			
-			let verticalRect = {
-            x: this.x,
-			y: this.y + this.yspeed,
-			width: this.width,
-			height: this.height
-			}
-			
-			//check for intersections
-			for (let i = 0; i < borders.length; i++) {
-			   let	borderRect = {
-				   x: borders[i].x,
-				   y: borders[i].y,
-				   width: borders[i].width,
-				   height: borders[i].height
-				   type: borders[i].type
-				   
-			   }
-			   
-			   if(checkIntersection(horizontalRect, borderRect)){
-				   while(checkIntersection(horizontalRect, borderRect)){
-					   horizontalRect.x -= Math.sign(this.xspeed);	   
-				   }
-			     
-			   this.x = horizontalRect.x
-			   this.xspeed = 0
-			   }
-			      if(checkIntersection(verticalRect, borderRect)){
-				   while(checkIntersection(verticalRect, borderRect)){
-					   verticalRect.y -= Math.sign(this.yspeed);
-			   }
-			   this.y = verticalRect.y
-			   isGrounded = true
-			   canDoubleJump = true
-			   this.yspeed = 0
-			 
-			  
-			   }
-			   			   
-			   
-			}
-			obstacleMover = this.xspeed
-			this.y += this.yspeed
-        }
-    };
+				height: this.height,
+			};
 
-    this.draw = function() {
-		
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    };
+			let verticalRect = {
+				x: this.x,
+				y: this.y + this.yspeed,
+				width: this.width,
+				height: this.height,
+			};
+
+			for (let i = 0; i < borders.length; i++) {
+				
+
+				if (checkIntersection(horizontalRect, borders[i])) {
+					while (checkIntersection(horizontalRect, borders[i])) {
+						horizontalRect.x -= Math.sign(this.xspeed);
+					}
+					this.x = horizontalRect.x;
+					this.xspeed = 0;
+				}
+
+				if (checkIntersection(verticalRect, borders[i])) {
+					while (checkIntersection(verticalRect, borders[i])) {
+						
+						verticalRect.y -= Math.sign(this.yspeed);
+					}
+					this.y = verticalRect.y;
+					isGrounded = true;
+					
+					canDoubleJump = true;
+					this.yspeed = 0;
+				}
+			}
+
+			obstacleMover = this.xspeed;
+			this.y += this.yspeed;
+		}
+	}
+
+	draw() {
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+	
+	gameOver(){
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, 1280, 720)
+	ctx.fillStyle = "red"
+	ctx.font = "80px impact"
+	ctx.fillText("game over", 450, 300)
+    }
 }
